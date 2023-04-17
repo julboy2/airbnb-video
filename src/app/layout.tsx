e21d24006fc66d9bf343be7e5a42b2b7d86d798e
huk1183 <huk1183@getplus.co.kr>
2023-04-17 17:37:55 +0900

    registermodal 작성중

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,7 @@ import { Nunito } from 'next/font/google'
 
 import './globals.css'
 import Narbar from '@/components/navbar/Narbar'
-import Model from '@/components/modals/Modal'
+import RegisterModal from '@/components/modals/RegisterModal'
 
 export const metadata = {
   title: 'Airbnb',
@@ -22,7 +22,7 @@ export default function RootLayout({
     <html lang="en">
       <body className={font.className}>
         {/* 만약 hydration error 에러가 난다면 ClientOnly 로 감싸줄것 */}
-        <Model title="Hello World" actionLabel="Submit" isOpen />
+        <RegisterModal />
         <Narbar /> {children}
       </body>
     </html>
