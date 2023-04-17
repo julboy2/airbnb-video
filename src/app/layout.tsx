bb90126f8d28687bf42dabe65422c2278174152b
huk1183 <huk1183@getplus.co.kr>
2023-04-17 17:00:48 +0900

    add modal

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import { Nunito } from 'next/font/google'
 
 import './globals.css'
 import Narbar from '@/components/navbar/Narbar'
+import Model from '@/components/modals/Modal'
 
 export const metadata = {
   title: 'Airbnb',
@@ -20,6 +21,8 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={font.className}>
+        {/* 만약 hydration error 에러가 난다면 ClientOnly 로 감싸줄것 */}
+        <Model title="Hello World" actionLabel="Submit" isOpen />
         <Narbar /> {children}
       </body>
     </html>
