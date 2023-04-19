ed5f2bee807d877ca8036784dce2246746aea247
JamesWoo <JamesWoo>
2023-04-19 22:19:19 +0900

    credentials login finish

--- src/app/api/register/route.ts
+++ src/app/api/register/route.ts
@@ -2,15 +2,12 @@ import { NextRequest, NextResponse } from 'next/server'
 import bcrypt from 'bcrypt'
 import prisma from '@/libs/prismadb'
 
-// import { PrismaClient, Prisma } from '@prisma/client'
-// const prisma = new PrismaClient()
-
 export async function POST(req: NextRequest) {
   const body = await req.json()
   const { email, name, password } = body
 
   const hashedPassword = await bcrypt.hash(password, 12)
-  console.log(hashedPassword)
+
   const user = await prisma.user.create({
     data: {
       email,
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,8 @@ import './globals.css'
 import Narbar from '@/components/navbar/Narbar'
 import RegisterModal from '@/components/modals/RegisterModal'
 import ToasterProvider from '@/providers/ToasterProvider'
+import LoginModal from '@/components/modals/LoginModal'
+import getCurrentUser from '@/actions/getCurrentUser'
 
 export const metadata = {
   title: 'Airbnb',
@@ -14,18 +16,20 @@ const font = Nunito({
   subsets: ['latin'],
 })
 
-export default function RootLayout({
+export default async function RootLayout({
   children,
 }: {
   children: React.ReactNode
 }) {
+  const currentUser = await getCurrentUser()
   return (
     <html lang="en">
       <body className={font.className}>
         {/* 만약 hydration error 에러가 난다면 ClientOnly 로 감싸줄것 */}
         <ToasterProvider />
+        <LoginModal />
         <RegisterModal />
-        <Narbar /> {children}
+        <Narbar currentUser={currentUser} /> {children}
       </body>
     </html>
   )
