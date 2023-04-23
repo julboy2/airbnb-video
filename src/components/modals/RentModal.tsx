3c6e1f959e37c412797490bd422700e98be8f3d3
JamesWoo <JamesWoo>
2023-04-23 21:30:04 +0900

    react-leaflet

--- src/components/modals/RentModal.tsx
+++ src/components/modals/RentModal.tsx
@@ -7,6 +7,8 @@ import Heading from '../Heading'
 import { categories } from '../navbar/Categories'
 import CategoryInput from '../inputs/CategoryInput'
 import { FieldValues, useForm } from 'react-hook-form'
+import CountrySelect from '../inputs/CountrySelect'
+import Map from '../Map'
 
 enum STEPS {
   CATEGORY = 0,
@@ -43,6 +45,15 @@ export default function RentModal() {
   })
 
   const category = watch('category')
+  const location = watch('location')
+
+  const setCustomValue = (id: string, value: any) => {
+    setValue(id, value, {
+      shouldValidate: true,
+      shouldDirty: true,
+      shouldTouch: true,
+    })
+  }
 
   const onBack = () => {
     setStep((value) => value - 1)
@@ -78,8 +89,8 @@ export default function RentModal() {
         {categories.map((item) => (
           <div key={item.label} className="col-span-1">
             <CategoryInput
-              onClick={() => {}}
-              selected={false}
+              onClick={(category) => setCustomValue('category', category)}
+              selected={category === item.label}
               label={item.label}
               icon={item.icon}
             />
@@ -89,11 +100,27 @@ export default function RentModal() {
     </div>
   )
 
+  if (step === STEPS.LOCATION) {
+    bodyContent = (
+      <div className="flex flex-col gap-8">
+        <Heading
+          title="Where is your place locatied?"
+          subtitle="Help guests find you!"
+        />
+        <CountrySelect
+          value={location}
+          onChange={(value) => setCustomValue('location', value)}
+        />
+        <Map />
+      </div>
+    )
+  }
+
   return (
     <Modal
       isOpen={rentModal.isOpen}
       onClose={rentModal.onClose}
-      onSubmit={rentModal.onClose}
+      onSubmit={onNext}
       actionLabel={actionLabel}
       secondaryActionLabel={secondaryActionLabel}
       secondaryAction={step === STEPS.CATEGORY ? undefined : onBack}
